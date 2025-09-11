5b3dfff349be6675561ddb71edb6f3fa19d9bc9a
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-09-11 11:35:44 +1000

    wip

--- src/rpc/handlers.rs
+++ src/rpc/handlers.rs
@@ -1,7 +1,7 @@
 use jsonrpsee::Extensions;
 use jsonrpsee::core::RpcResult;
 
-use super::types::{CommitmentRequest, SignedCommitment, SlotInfoResponse, FeeInfo, Commitment};
+use super::types::{Commitment, CommitmentRequest, FeeInfo, SignedCommitment, SlotInfoResponse};
 
 pub fn commitment_request_handler(
 	params: jsonrpsee::types::Params<'_>,
@@ -9,7 +9,7 @@ pub fn commitment_request_handler(
 	_extensions: &Extensions,
 ) -> RpcResult<SignedCommitment> {
 	let request: CommitmentRequest = params.parse()?;
-	
+
 	// TODO: Implement actual commitment logic
 	let commitment = Commitment {
 		commitment_type: request.commitment_type,
@@ -17,12 +17,12 @@ pub fn commitment_request_handler(
 		request_hash: "0x0000000000000000000000000000000000000000000000000000000000000000".to_string(),
 		slasher: request.slasher,
 	};
-	
+
 	let signed_commitment = SignedCommitment {
 		commitment,
 		signature: "0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
 	};
-	
+
 	Ok(signed_commitment)
 }
 
@@ -32,7 +32,7 @@ pub fn commitment_result_handler(
 	_extensions: &Extensions,
 ) -> RpcResult<SignedCommitment> {
 	let request_hash: String = params.one()?;
-	
+
 	// TODO: Implement actual commitment retrieval logic
 	let commitment = Commitment {
 		commitment_type: 1,
@@ -40,12 +40,12 @@ pub fn commitment_result_handler(
 		request_hash,
 		slasher: "0x0000000000000000000000000000000000000000".to_string(),
 	};
-	
+
 	let signed_commitment = SignedCommitment {
 		commitment,
 		signature: "0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
 	};
-	
+
 	Ok(signed_commitment)
 }
 
@@ -55,10 +55,8 @@ pub fn slots_handler(
 	_extensions: &Extensions,
 ) -> RpcResult<SlotInfoResponse> {
 	// TODO: Implement actual slots logic
-	let response = SlotInfoResponse {
-		slots: vec![],
-	};
-	
+	let response = SlotInfoResponse { slots: vec![] };
+
 	Ok(response)
 }
 
@@ -68,12 +66,9 @@ pub fn fee_handler(
 	_extensions: &Extensions,
 ) -> RpcResult<FeeInfo> {
 	let request: CommitmentRequest = params.parse()?;
-	
+
 	// TODO: Implement actual fee calculation logic
-	let fee_info = FeeInfo {
-		fee_payload: vec![0u8; 32],
-		commitment_type: request.commitment_type,
-	};
-	
+	let fee_info = FeeInfo { fee_payload: vec![0u8; 32], commitment_type: request.commitment_type };
+
 	Ok(fee_info)
 }
